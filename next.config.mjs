0629b523fdfcc8b6d5e49e91a858e09411079cf9
unknown <tttrai98@gmail.com>
2024-05-08 11:05:14 +0700

    final fix authmiddleware again ZZZZ

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
