2ccf084a8b5a7a641fbd462ee6c8382871217cea
unknown <tttrai98@gmail.com>
2024-05-08 10:56:47 +0700

    final fix authmiddleware again

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,4 @@ export default authMiddleware({
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-  unstable_allowDynamic: [
-    // allows a single file
-    "/lib/utils.js",
-    // use a glob to allow anything in the function-bind 3rd party module
-    "/node_modules/function-bind/**",
-  ],
 };
