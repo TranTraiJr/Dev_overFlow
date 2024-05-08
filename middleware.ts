2780e467f345b5508cfcf9d159a17a96172d06fa
unknown <tttrai98@gmail.com>
2024-05-08 10:09:18 +0700

    final fix all tailwind agianaaaaa

--- middleware.ts
+++ middleware.ts
@@ -6,4 +6,10 @@ export default authMiddleware({
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
+  unstable_allowDynamic: [
+    // allows a single file
+    "/lib/utils.js",
+    // use a glob to allow anything in the function-bind 3rd party module
+    "/node_modules/function-bind/**",
+  ],
 };
