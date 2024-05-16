e3aa97e515b5c990e56805c80982f21413bc7635
unknown <tttrai98@gmail.com>
2024-05-16 20:34:13 +0700

    final version 3

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -7,12 +7,12 @@ import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatNumber, getTimestamp } from "@/lib/utils";
-import { ParamsProps } from "@/types";
+import { URLProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
-const Page = async ({ params, searchParams }: ParamsProps) => {
+const Page = async ({ params, searchParams }: URLProps) => {
   const result = await getQuestionById({ questionId: params.id });
   const { userId: clerkId } = auth();
 
